f25017d794f5570922c8208717892f617b79322f
fabrixiop26 <fabriciopuche@hotmail.com>
2022-10-25 17:37:12 -0500

    remove readonly attribute from ids

--- DTOModels/CharacterDTO.cs
+++ DTOModels/CharacterDTO.cs
@@ -2,14 +2,12 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-using Swashbuckle.AspNetCore.Annotations;
 
 namespace GameRental.DTOModels
 {
     [DisplayName("CharacterViewModel")]
     public class CharacterDTO
     {
-        [SwaggerSchema(ReadOnly = true)]
         public int CharacterId { get; set; }
         [StringLength(50)]
         [Unicode(false)]
